f51392a2c69e3edeab3691a6b2efbf9ad71f99c8
caisr <383911973@qq.com>
2019-09-02 22:24:33 +0800

    支持scss语法

--- pages/index.vue
+++ pages/index.vue
@@ -30,11 +30,19 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
+  },
+  async asyncData({ params, error, app: { $axios } }) {
+    try {
+      await Promise.resolve(10)
+      console.log(10)
+    } catch (e) {
+      error({ statusCode: 404, message: 'Post not found' })
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -42,27 +50,27 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system,
+      BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+      sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  .links {
+    padding-top: 15px;
+  }
 }
 </style>
